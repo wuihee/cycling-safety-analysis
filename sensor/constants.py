e5ee9862fc2a196fbc758ccd03c32658fd13e981
wuihee <wuihee@gmail.com>
2023-09-03 00:28:20 +0800

    Major refactoring

--- sensor/constants.py
+++
@@ -1,8 +0,0 @@
-class AWSConstants:
-    ENDPOINT = "a1cqysdrxx4ce4-ats.iot.ap-southeast-2.amazonaws.com"
-    PORT = 8883
-    CLIENT_ID = "tof_sensor"
-    TOPIC = "TOF"
-    AWS_CERTIFICATE = "./certs/device.pem.crt"
-    AWS_PRIVATE_KEY = "./certs/private.pem.key"
-    AWS_ROOT_CA = "./certs/Amazon-root-CA-1.pem"
--- sensor/laser_test.py
+++
@@ -1,6 +0,0 @@
-from laser_sensor import LaserSensor
-from publish import Publisher
-
-publisher = Publisher()
-sensor = LaserSensor(publisher)
-sensor.measure_distance_continuous()
