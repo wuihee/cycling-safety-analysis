39befb0ca143929ff443d364c876d46a78a21f20
wuihee <wuihee@gmail.com>
2023-09-05 23:11:30 +0800

    Updated data formatting

--- src/format_data.py
+++ src/format_data.py
@@ -1,46 +1,47 @@
-"""
-Throughout this project, I collected data from the sensors through different
-means, from which the data was stored in different formats. The idea of this
-module is to reformat each set of data in a fixed structure. Distance
-measurements by each sensor should be stored in a text file, each line
-representing a point of measurement. Each line should be formatted as such:
-
-TIME DISTANCE SIGNAL_STRENGTH
-
-where, time, distance, and signal strength are values separated by a space.
-"""
-
-import os
-import pathlib
+from pathlib import Path
 
 from format import laser, tof
 
-os.chdir(os.path.realpath(os.path.dirname(__file__)))
+# Base Directories
+BASE_RAW_DATA = Path("./data/raw")
+BASE_DATA = Path("./data/processed")
 
-# Basic TOF indoors and outdoors test by software stored in excel files.
-TOF_INDOORS_RAW_DATA = pathlib.Path("./raw_data/tof_basic_tests/indoors")
-TOF_OUTDOORS_RAW_DATA = pathlib.Path("./raw_data/tof_basic_tests/outdoors")
-TOF_INDOORS_DATA = pathlib.Path("./data/tof_basic_tests/indoors")
-TOF_OUTDOORS_DATA = pathlib.Path("./data/tof_basic_tests/outdoors")
-tof.format_excel(TOF_INDOORS_RAW_DATA, TOF_INDOORS_DATA)
-tof.format_excel(TOF_OUTDOORS_RAW_DATA, TOF_OUTDOORS_DATA)
+# Formatting TOF Basic Tests
+TOF_BASE_RAW = BASE_RAW_DATA / "tof_basic_tests"
+TOF_BASE_DATA = BASE_DATA / "tof_basic_tests"
+tof_in_raw_pairs = [
+    (TOF_BASE_RAW / "indoors", TOF_BASE_DATA / "indoors"),
+    (TOF_BASE_RAW / "outdoors", TOF_BASE_DATA / "outdoors"),
+    (TOF_BASE_RAW / "with_shade", TOF_BASE_DATA / "with_shade"),
+]
+for raw, data in tof_in_raw_pairs:
+    if "with_shade" in str(raw):
+        tof.format_text(raw, data)
+    else:
+        tof.format_excel(raw, data)
 
-# Basic outdoors test with shade by Raspberry Pi stored in text file.
-TOF_WITH_SHADE_RAW_DATA = pathlib.Path("./raw_data/tof_basic_tests/with_shade")
-TOF_WITH_SHADE_DATA = pathlib.Path("./data/tof_basic_tests/with_shade")
-tof.format_text(TOF_WITH_SHADE_RAW_DATA, TOF_WITH_SHADE_DATA)
+# Formatting Laser Basic Tests
+LASER_BASE_RAW = BASE_RAW_DATA / "laser_basic_tests"
+LASER_BASE_DATA = BASE_DATA / "laser_basic_tests"
+laser_in_raw_pairs = [
+    (LASER_BASE_RAW / "indoors", LASER_BASE_DATA / "indoors"),
+    (LASER_BASE_RAW / "outdoors", LASER_BASE_DATA / "outdoors"),
+]
+for raw, data in laser_in_raw_pairs:
+    if "indoors" in str(raw):
+        laser.format_protocol_data(raw, data)
+    else:
+        tof.format_text(raw, data)
 
-# Basic laser indoor test by software stored in text file.
-LASER_INDOORS_RAW_DATA = pathlib.Path("./raw_data/laser_basic_tests/indoors")
-LASER_INDOORS_DATA = pathlib.Path("./data/laser_basic_tests/indoors")
-laser.format_protocol_data(LASER_INDOORS_RAW_DATA, LASER_INDOORS_DATA)
+# Formatting Laser Outdoor Tests
+laser.format_ascii_data(
+    BASE_RAW_DATA / "laser_outdoor_tests", BASE_DATA / "laser_outdoor_tests"
+)
 
-# Basic laser outdoor test by Raspberry Pi stored in text file.
-LASER_OUTDOORS_RAW_DATA = pathlib.Path("./raw_data/laser_basic_tests/outdoors")
-LASER_OUTDOORS_DATA = pathlib.Path("./data/laser_basic_tests/outdoors")
-tof.format_text(LASER_OUTDOORS_RAW_DATA, LASER_OUTDOORS_DATA)
-
-# Outdoors stationary test by software stored in text file.
-LASER_CYCLING_TEST_RAW_DATA = pathlib.Path("./raw_data/laser_outdoor_tests/")
-LASER_CYCLING_TEST_DATA = pathlib.Path("./data/laser_outdoor_tests")
-laser.format_ascii_data(LASER_CYCLING_TEST_RAW_DATA, LASER_CYCLING_TEST_DATA)
+# Formatting LIDAR Basic Tests
+LIDAR_BASE_RAW = BASE_RAW_DATA / "lidar_basic_tests"
+LIDAR_BASE_DATA = BASE_DATA / "lidar_basic_tests"
+lidar_in_raw_pairs = [(LIDAR_BASE_RAW / "indoors", LIDAR_BASE_DATA / "indoors")]
+for raw, data in lidar_in_raw_pairs:
+    if "indoors" in str(raw):
+        tof.format_text(raw, data)
